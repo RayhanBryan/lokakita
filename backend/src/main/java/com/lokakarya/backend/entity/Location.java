257460bd17b2d928872146fdf99c9d0c2ff9439c
DESKTOP-PCDMJK8\E7450 <galihsahar21@gmail.com>
2022-04-18 09:36:01 +0700

    add customer entity

--- backend/src/main/java/com/lokakarya/backend/entity/Location.java
+++ backend/src/main/java/com/lokakarya/backend/entity/Location.java
@@ -1,11 +1,79 @@
 package com.lokakarya.backend.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "LOCATIONS")
 public class Location {
+
+    private Long locationId;
+    private String streetAddress;
+    private String postalCode;
+    private String city;
+    private String stateProvince;
+    private Date createdDate;
+    private Country country;
+
+    @Id
+    @GeneratedValue(generator = "LOCACTION_GEN", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "LOCACTION_GEN", sequenceName = "LOCATIONS_SEQ_03", initialValue = 1, allocationSize = 1)
+    public Long getLocationId() {
+        return locationId;
+    }
+    public void setLocationId(Long locationId) {
+        this.locationId = locationId;
+    }
+
+    @Column(name = "STREET_ADDRESS")
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    @Column(name = "POSTAL_CODE")
+    public String getPostalCode() {
+        return postalCode;
+    }
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    @Column(name = "CITY")
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Column(name = "STATE_PROVINCE")
+    public String getStateProvince() {
+        return stateProvince;
+    }
+    public void setStateProvince(String stateProvince) {
+        this.stateProvince = stateProvince;
+    }
+
+    @Column(name = "CREATED_DATE")
+    @Temporal(TemporalType.DATE)
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "COUNTRY_ID")
+    public Country getCountry() {
+        return country;
+    }
+    public void setCountry(Country country) {
+        this.country = country;
+    }
 
 
 }
