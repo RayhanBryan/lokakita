72227a18915fdfb1f68a02c9fbefe06284b9aa23
DESKTOP-00LSPAA\E7450 <lukypancap@gmail.com>
2022-04-18 14:48:24 +0700

    edit location service find by street address

--- backend/src/main/java/com/lokakarya/backend/repository/LocationRepository.java
+++ backend/src/main/java/com/lokakarya/backend/repository/LocationRepository.java
@@ -1,6 +1,8 @@
 package com.lokakarya.backend.repository;
 
 
+import java.util.Optional;
+
 import com.lokakarya.backend.entity.Location;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,5 +11,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface LocationRepository extends JpaRepository<Location, Long> {
 
     Page<Location> findAll(Pageable page);
+    Optional<Location> findByStreetAddressContainingIgnoreCase(String streetAddress);
     Page<Location> findBystreetAddressContainingIgnoreCase(String streetAddress, Pageable paging);
 }
