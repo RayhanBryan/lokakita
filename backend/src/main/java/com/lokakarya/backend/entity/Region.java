b0ae3810dfb651ff256668bb5faa2c70f6c975d8
DESKTOP-E3MS7GH\E7450 <naviatus.sholekah@gmail.com>
2022-04-18 11:18:49 +0700

    edit region entity

--- backend/src/main/java/com/lokakarya/backend/entity/Region.java
+++ backend/src/main/java/com/lokakarya/backend/entity/Region.java
@@ -8,22 +8,22 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "REGIONS")
 public class Region {
-    private Long regionIdLong;
-    private String regionNameString;
+    private Long regionId;
+    private String regionName;
 
     @Id
     @Column(name = "REGION_ID")
-    public Long getRegionIdLong() {
-        return regionIdLong;
+    public Long getRegionId() {
+        return regionId;
     }
-    public void setRegionIdLong(Long regionLong) {
-        this.regionIdLong = regionLong;
+    public void setRegionId(Long region) {
+        this.regionId = region;
     }
     @Column(name = "REGION_NAME")
-    public String getRegionNameString() {
-        return regionNameString;
+    public String getRegionName() {
+        return regionName;
     }
-    public void setRegionNameString(String regionNameString) {
-        this.regionNameString = regionNameString;
+    public void setRegionName(String regionName) {
+        this.regionName = regionName;
     }
 }
