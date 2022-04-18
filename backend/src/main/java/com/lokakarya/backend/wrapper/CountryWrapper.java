aa61db92032827c4d29a9a423193fc30c87b3cdd
DESKTOP-E3MS7GH\E7450 <naviatus.sholekah@gmail.com>
2022-04-18 10:24:51 +0700

    Add job,  jobHist, country wrapper

---
+++ backend/src/main/java/com/lokakarya/backend/wrapper/CountryWrapper.java
@@ -0,0 +1,33 @@
+package com.lokakarya.backend.wrapper;
+
+public class CountryWrapper {
+    private String countryId;
+    private String countryName;
+    private Long regionId;
+    private String regionName;
+
+    public String getCountryId() {
+        return countryId;
+    }
+    public void setCountryId(String countryId) {
+        this.countryId = countryId;
+    }
+    public String getCountryName() {
+        return countryName;
+    }
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+    public Long getRegionId() {
+        return regionId;
+    }
+    public void setRegionId(Long regionId) {
+        this.regionId = regionId;
+    }
+    public String getRegionName() {
+        return regionName;
+    }
+    public void setRegionName(String regionName) {
+        this.regionName = regionName;
+    }
+}
---
+++ backend/src/main/java/com/lokakarya/backend/wrapper/JobHistoryWrapper.java
@@ -0,0 +1,55 @@
+package com.lokakarya.backend.wrapper;
+import java.util.Date;
+
+public class JobHistoryWrapper {
+    private Long employeeId;
+	private Date startDate;
+	private Date endDate;
+	private String jobId;
+	private String jobTitle;
+	private Long departmentId;
+	private String departmentName;
+	
+	public Long getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(Long employeeId) {
+		this.employeeId = employeeId;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public String getJobId() {
+		return jobId;
+	}
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+	public String getJobTitle() {
+		return jobTitle;
+	}
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+}
