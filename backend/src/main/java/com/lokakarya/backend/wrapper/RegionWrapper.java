88c8f4fb067ed8906fb2bf2d0d934ce177ba5ccd
DESKTOP-00LSPAA\E7450 <lukypancap@gmail.com>
2022-04-18 11:13:40 +0700

    RegionWrapper Added

---
+++ backend/src/main/java/com/lokakarya/backend/wrapper/RegionWrapper.java
@@ -0,0 +1,21 @@
+package com.lokakarya.backend.wrapper;
+
+public class RegionWrapper {
+    
+    private Long regionId;
+	private String regionName;
+	
+	public Long getRegionId() {
+		return regionId;
+	}
+	public void setRegionId(Long regionId) {
+		this.regionId = regionId;
+	}
+	public String getRegionName() {
+		return regionName;
+	}
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
+	}
+}
+
