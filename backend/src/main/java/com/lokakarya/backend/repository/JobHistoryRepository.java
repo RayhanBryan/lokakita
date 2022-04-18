fbf8446467cc82ad7ac284f63c2f6d0d89b42a3c
DESKTOP-E3MS7GH\E7450 <naviatus.sholekah@gmail.com>
2022-04-18 10:18:37 +0700

    edit jobHist repo

--- backend/src/main/java/com/lokakarya/backend/repository/JobHistoryRepository.java
+++ backend/src/main/java/com/lokakarya/backend/repository/JobHistoryRepository.java
@@ -1,5 +1,7 @@
 package com.lokakarya.backend.repository;
 
-public class JobHistoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.lokakarya.backend.entity.JobHistory;
+public interface JobHistoryRepository extends JpaRepository<JobHistory,Long> {
     
 }
