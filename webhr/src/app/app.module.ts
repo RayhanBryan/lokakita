fc99b7d9bf5733ae24b33a97703f7fef90a42dba
RayhanBryan <rayhanbryan@gmail.com>
2022-04-19 20:27:29 +0700

    Nambah side bar seperti request galih

--- webhr/src/app/app.module.ts
+++ webhr/src/app/app.module.ts
@@ -32,11 +32,12 @@ import { CalendarModule } from 'primeng/calendar';
 import { DatamasterComponent } from './components/datamaster/datamaster.component';
 import { CheckboxModule } from 'primeng/checkbox';
 import { NotfoundComponent } from './components/notfound/notfound.component';
-
+import { SidebarModule } from 'primeng/sidebar';
 import { MessagesModule } from 'primeng/messages';
 import { MessageModule } from 'primeng/message';
 import { BonusComponent } from './components/bonus/bonus.component';
 import { CustomerComponent } from './components/customer/customer.component';
+import { TieredMenuModule } from 'primeng/tieredmenu';
 
 
 @NgModule({
@@ -79,7 +80,9 @@ import { CustomerComponent } from './components/customer/customer.component';
     CheckboxModule,
     CalendarModule,
     MessagesModule,
-    MessageModule
+    MessageModule,
+    TieredMenuModule,
+    SidebarModule
   ],
   providers: [ConfirmationService],
   bootstrap: [AppComponent],
