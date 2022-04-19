4d209ae2afc23c81ed13e5af09f6a1f226c05cb1
DESKTOP-PCSOFBC\dell <cindyhilma1998@gmail.com>
2022-04-19 15:51:49 +0700

    fix app module

--- webhr/src/app/app.module.ts
+++ webhr/src/app/app.module.ts
@@ -36,11 +36,7 @@ import { NotfoundComponent } from './components/notfound/notfound.component';
 import { MessagesModule } from 'primeng/messages';
 import { MessageModule } from 'primeng/message';
 import { BonusComponent } from './components/bonus/bonus.component';
-<<<<<<< HEAD
 import { CustomerComponent } from './components/customer/customer.component';
-=======
-import { SidebarModule } from 'primeng/sidebar';
->>>>>>> 5a9e937d8c1e877a8b816f4f355896c1e339b405
 
 
 @NgModule({
@@ -83,8 +79,7 @@ import { SidebarModule } from 'primeng/sidebar';
     CheckboxModule,
     CalendarModule,
     MessagesModule,
-    MessageModule,
-    SidebarModule
+    MessageModule
   ],
   providers: [ConfirmationService],
   bootstrap: [AppComponent],
