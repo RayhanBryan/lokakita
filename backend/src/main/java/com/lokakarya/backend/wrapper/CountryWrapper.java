a554e188becdd3998faa7e4760b040e81fff91b9
DESKTOP-E3MS7GH\E7450 <naviatus.sholekah@gmail.com>
2022-04-19 13:05:25 +0700

    cancel edited

--- backend/src/main/java/com/lokakarya/backend/wrapper/CountryWrapper.java
+++ backend/src/main/java/com/lokakarya/backend/wrapper/CountryWrapper.java
@@ -1,37 +1,37 @@
 package com.lokakarya.backend.wrapper;
-import lombok.Getter;
-import lombok.Setter;
+// import lombok.Getter;
+// import lombok.Setter;
 
-@Getter
-@Setter
+// @Getter
+// @Setter
 public class CountryWrapper {
     private String countryId;
     private String countryName;
     private Long regionId;
     private String regionName;
 
-    // public String getCountryId() {
-    //     return countryId;
-    // }
-    // public void setCountryId(String countryId) {
-    //     this.countryId = countryId;
-    // }
-    // public String getCountryName() {
-    //     return countryName;
-    // }
-    // public void setCountryName(String countryName) {
-    //     this.countryName = countryName;
-    // }
-    // public Long getRegionId() {
-    //     return regionId;
-    // }
-    // public void setRegionId(Long regionId) {
-    //     this.regionId = regionId;
-    // }
-    // public String getRegionName() {
-    //     return regionName;
-    // }
-    // public void setRegionName(String regionName) {
-    //     this.regionName = regionName;
-    // }
+    public String getCountryId() {
+        return countryId;
+    }
+    public void setCountryId(String countryId) {
+        this.countryId = countryId;
+    }
+    public String getCountryName() {
+        return countryName;
+    }
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+    public Long getRegionId() {
+        return regionId;
+    }
+    public void setRegionId(Long regionId) {
+        this.regionId = regionId;
+    }
+    public String getRegionName() {
+        return regionName;
+    }
+    public void setRegionName(String regionName) {
+        this.regionName = regionName;
+    }
 }
