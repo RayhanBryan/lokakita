f92e117964d0657d1669d95dfeb8d840f26d260f
prawiroht <prawiroht@gmail.com>
2022-04-19 13:27:32 +0700

    benerin error lombok

--- backend/src/main/java/com/lokakarya/backend/entity/User.java
+++ backend/src/main/java/com/lokakarya/backend/entity/User.java
@@ -12,13 +12,15 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "USERS")
-@Getter
-@Setter
+// @Data
+// @NoArgsConstructor
 public class User {
 
     @Id
@@ -26,7 +28,6 @@ public class User {
     @SequenceGenerator(name = "USERS_GEN", sequenceName = "USER_SEQ",initialValue = 1, allocationSize = 1)
     @Column(name = "USER_ID")
     private Long userId;
-
     @Column(name = "USERNAME")
     private String username;
     @Column(name = "PASSWORD")
@@ -51,89 +52,79 @@ public class User {
     private Date updatedDate;
     @Column(name = "UPDATED_BY")
     private String updatedBy;
+
+    public Long getUserId() {
+        return userId;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getProgramName() {
+        return programName;
+    }
+    public void setProgramName(String programName) {
+        this.programName = programName;
+    }
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+    public String getCreatedBy() {
+        return createdBy;
+    }
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
     
-    
-    // public Long getUserId() {
-    //     return userId;
-    // }
-    // public void setUserId(Long userId) {
-    //     this.userId = userId;
-    // }
-
-    // public String getUsername() {
-    //     return username;
-    // }
-    // public void setUsername(String username) {
-    //     this.username = username;
-    // }
-
-    // public String getPassword() {
-    //     return password;
-    // }
-    // public void setPassword(String password) {
-    //     this.password = password;
-    // }
-
-    // public String getName() {
-    //     return name;
-    // }
-    // public void setName(String name) {
-    //     this.name = name;
-    // }
-
-    // public String getAddress() {
-    //     return address;
-    // }
-    // public void setAddress(String address) {
-    //     this.address = address;
-    // }
-
-    // public String getEmail() {
-    //     return email;
-    // }
-    // public void setEmail(String email) {
-    //     this.email = email;
-    // }
-
-    // public String getPhone() {
-    //     return phone;
-    // }
-    // public void setPhone(String phone) {
-    //     this.phone = phone;
-    // }
-
-    // public String getProgramName() {
-    //     return programName;
-    // }
-    // public void setProgramName(String programName) {
-    //     this.programName = programName;
-    // }
-
-    // public Date getCreatedDate() {
-    //     return createdDate;
-    // }
-    // public void setCreatedDate(Date createdDate) {
-    //     this.createdDate = createdDate;
-    // }
-
-    // public String getCreatedBy() {
-    //     return createdBy;
-    // }
-    // public void setCreatedBy(String createdBy) {
-    //     this.createdBy = createdBy;
-    // }
-
-    // public Date getUpdatedDate() {
-    //     return updatedDate;
-    // }
-    // public void setUpdatedDate(Date updatedDate) {
-    //     this.updatedDate = updatedDate;
-    // }
-
-    // public String getUpdatedBy() {
-    //     return updatedBy;
-    // }
-    // public void setUpdatedBy(String updatedBy) {
-    //     this.updatedBy = updatedBy;
-    // }
 }
--- backend/src/main/java/com/lokakarya/backend/wrapper/UserWrapper.java
+++ backend/src/main/java/com/lokakarya/backend/wrapper/UserWrapper.java
@@ -5,8 +5,8 @@ import java.util.Date;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+// @Getter
+// @Setter
 public class UserWrapper {
     private Long userId;
     private String username;
@@ -21,76 +21,76 @@ public class UserWrapper {
     private Date updatedDate;
     private String updatedBy;
     
-    // public Long getUserId() {
-    //     return userId;
-    // }
-    // public void setUserId(Long userId) {
-    //     this.userId = userId;
-    // }
-    // public String getUsername() {
-    //     return username;
-    // }
-    // public void setUsername(String username) {
-    //     this.username = username;
-    // }
-    // public String getPassword() {
-    //     return password;
-    // }
-    // public void setPassword(String password) {
-    //     this.password = password;
-    // }
-    // public String getName() {
-    //     return name;
-    // }
-    // public void setName(String name) {
-    //     this.name = name;
-    // }
-    // public String getAddress() {
-    //     return address;
-    // }
-    // public void setAddress(String address) {
-    //     this.address = address;
-    // }
-    // public String getEmail() {
-    //     return email;
-    // }
-    // public void setEmail(String email) {
-    //     this.email = email;
-    // }
-    // public String getPhone() {
-    //     return phone;
-    // }
-    // public void setPhone(String phone) {
-    //     this.phone = phone;
-    // }
-    // public String getProgramName() {
-    //     return programName;
-    // }
-    // public void setProgramName(String programName) {
-    //     this.programName = programName;
-    // }
-    // public Date getCreatedDate() {
-    //     return createdDate;
-    // }
-    // public void setCreatedDate(Date createdDate) {
-    //     this.createdDate = createdDate;
-    // }
-    // public String getCreatedBy() {
-    //     return createdBy;
-    // }
-    // public void setCreatedBy(String createdBy) {
-    //     this.createdBy = createdBy;
-    // }
-    // public Date getUpdatedDate() {
-    //     return updatedDate;
-    // }
-    // public void setUpdatedDate(Date updatedDate) {
-    //     this.updatedDate = updatedDate;
-    // }
-    // public String getUpdatedBy() {
-    //     return updatedBy;
-    // }
-    // public void setUpdatedBy(String updatedBy) {
-    //     this.updatedBy = updatedBy;
-    // }
+    public Long getUserId() {
+        return userId;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getProgramName() {
+        return programName;
+    }
+    public void setProgramName(String programName) {
+        this.programName = programName;
+    }
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+    public String getCreatedBy() {
+        return createdBy;
+    }
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
 }
