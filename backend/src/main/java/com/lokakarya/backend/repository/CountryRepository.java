ed410cf05eacf866c12ececf5932db893b50a9df
DESKTOP-E3MS7GH\E7450 <naviatus.sholekah@gmail.com>
2022-04-21 09:01:16 +0700

    country repo edited

--- backend/src/main/java/com/lokakarya/backend/repository/CountryRepository.java
+++ backend/src/main/java/com/lokakarya/backend/repository/CountryRepository.java
@@ -14,14 +14,12 @@ public interface CountryRepository extends JpaRepository<Country, String>{
 	Page<Country> findByCountryNameContainingAllIgnoreCase(String countryName, Pageable paging);
 	Page<Country> findAll(Pageable page);
 	Page<Country> findByCountryNameContainingIgnoreCase(String countryName, Pageable paging);
-
 	List<Country> findByCountryNameContainingIgnoreCase(String countryName);
 	@Query(value = "SELECT * FROM COUNTRIES c  " +
-            "LEFT JOIN " +
-            "REGIONS r " +
+            "LEFT JOIN " + "REGIONS r " +
             "ON c.REGION_ID= " +
             "r.REGION_ID WHERE " +
             "LOWER(r.REGION_NAME) " +
-            "LIKE LOWER(CONCAT(CONCAT('%',:pRegionName), '%'))", nativeQuery = true)
+            "LIKE LOWER(CONCAT(CONCAT('%',:pRegionName), '%'))"+"ORDER BY COUNTRY_NAME ASC", nativeQuery = true)
     List<Country> findByRegionNameContainingIgnoreCase(@Param("pRegionName") String regionName);
 }
